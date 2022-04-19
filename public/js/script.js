d66bcff523915caf79aea78f0240c013456231ff
major jerk <shreyasrivastava28@gmail.com>
2022-04-19 14:20:49 +0530

    added comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 const search_via_api =()=>{
+    //here, we are fetching the data from the api and sending it via post
+    //request to server
     let key = "AIzaSyBdmHCJC-lBOYSf_4DL80Y8xBwj53xjxsw"
     let y_link = "https://www.googleapis.com/youtube/v3/search?part=snippet&key="+key+"&q=modern%20family%20series&type=video&maxResults=5&order=date"
     $.ajax({
@@ -32,19 +34,25 @@ const show_home = ()=>{
     $("input[name='search_query']").val("")
 }
 const search_video =()=>{
-    // alert($("input[name='search_query']").val())
     $("#home").css('display','none')
     $.ajax({
     method:"POST",
     url:"/search",
     data:{search_query:$("input[name='search_query']").val()},
     success:function(res){
+        if(res != "nothing"){
+
+        
             let str = `<iframe src='https://youtube.com/embed/${res.videos.videoId}' width="420"
 height="315" frameborder="0" allowfullscreen></iframe>`
             console.log(str)
         
         $("#search_results").append(str)
+        }
+        else{
+        $("#search_results").append("Try searching again with a different query")
 
+        }
     }
 })
 }
--- server.js
+++ server.js
@@ -8,20 +8,29 @@ dotenv.config({path:"./config.env"})
 mongoose.connect(process.env.DATABASE,{useNewUrlParser:true, useUnifiedTopology:true})
 const video = require("./models/video.js")
 app.set("view engine","ejs")
-express.static((path.join(__dirname, "/public")))
+const path = require("path")
+app.use(express.static((path.join(__dirname, "/public"))))
 
 app.get("/",async(req,res)=>{
     let doc = await video.find({}).sort({_id:-1})
     res.render("index",{videos:doc})
 })
 app.post("/search",async(req,res)=>{
+// we are searching for the video with the help of regex, so that if the user writes even half the title,
+// we are able to retrieve the video from database
 let doc = await video.find({title:{ $regex: req.body.search_query, $options: 'i' }})
+if(doc[0] != undefined)
 res.send({videos:doc[0]})
+else
+res.send("nothing")
 })
 app.post("/add-new-data",async(req,res)=>{
     let videos = req.body.videos
     
     let docc = await videos.find({videoId:videos[0].id.videoId})
+    //if the id of the latest video(videos[0]) obtained in the array(req.body.videos)
+    //is present in the database, we don't go into if-condition and directly move
+    //on to else-condition
     if(docc[0] == undefined){
         videos.items.forEach(async(vid) => {
             let doc = await video.find({videoId:vid.id.videoId})
